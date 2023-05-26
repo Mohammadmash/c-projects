3725cc62df92d4676d8be78f04331059b8f41120
Mohammad mashayekh <130070990+Mohammadmash@users.noreply.github.com>
2023-05-26 04:08:35 -0700

    Add files via upload

---
+++ number to triple/number to triple/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace number_to_triple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            Console.WriteLine("enter the number for triple");
+            string number = Console.ReadLine();
+            y = int.Parse(number);
+            y *= 3;
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ say hello/say hello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace say_hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("please write your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
